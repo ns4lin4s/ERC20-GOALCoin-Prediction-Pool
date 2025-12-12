4401f6a6c1f0c5aeb1ab300288e66511ef409fe9
Nelson Osvaldo Salinas Guajardo <ns4lin4s@gmail.com>
2025-12-11 23:56:12 -0300

    Restrict transfers to the same contract address
    Add new validation - ERC20 bugs

--- GOALCoin.sol
+++ GOALCoin.sol
@@ -9,4 +9,17 @@ contract GOALCoin is ERC20 {
     constructor(string memory name_, string memory symbol_) ERC20(name_,symbol_) {
         _mint(msg.sender, 1000*1e18);
     }
+
+    function _update(
+        address from,
+        address to,
+        uint256 amount
+    ) internal virtual override {
+        
+        // the address ('to') can't be the current contract address
+        // address(this) represent the address GOALCoin contract.
+        require(to != address(this), "GOALCoin: Cannot transfer tokens to the contract address.");
+        
+        super._update(from, to, amount);
+    }
 }
